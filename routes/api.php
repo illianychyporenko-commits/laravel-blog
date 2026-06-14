ebddb472e65837df0adeb3542a942ab74900a226
Ilya <illiaillia.nychyporenko@oa.edu.ua>
2026-06-14 20:20:17 +0000

    Лабораторна 3. Контроллери, роути, шаблони

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,11 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\Blog\PostController;
+
+Route::group(['namespace' => 'App\Http\Controllers\Api\Blog', 'prefix' => 'blog'], function () {
+    Route::apiResource('posts', PostController::class)->names('blog.posts');
+});
 
 Route::get('/user', function (Request $request) {
     return $request->user();
--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\RestTestController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::apiResource('rest', RestTestController::class)->names('restTest');
 
 Route::middleware([
     'auth:sanctum',
