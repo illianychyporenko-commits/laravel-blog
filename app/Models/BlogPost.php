57497015e4e5b779f767e7f12a44ba4b6d4284dd
Ilya <illiaillia.nychyporenko@oa.edu.ua>
2026-06-15 15:16:40 +0000

    Лабораторна 5. Колекції

--- app/Models/BlogPost.php
+++ app/Models/BlogPost.php
@@ -1,21 +1,10 @@
 <?php
 
 namespace App\Models;
- 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
-
-class BlogCategory extends Model
+class BlogPost extends Model
 {
     use SoftDeletes;
-    use HasFactory;
-
-    // Перевір, щоб тут не було друкарських помилок і все було всередині класу
-    protected $fillable = [
-        'title',
-        'slug',
-        'parent_id',
-        'description',
-    ];
 }
--- routes/web.php
+++ routes/web.php
@@ -2,12 +2,18 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\RestTestController;
+use App\Http\Controllers\DiggingDeeperController;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
 Route::apiResource('rest', RestTestController::class)->names('restTest');
+
+Route::group(['prefix' => 'digging_deeper'], function () {
+    Route::get('collections', [DiggingDeeperController::class, 'collections'])
+        ->name('digging_deeper.collections');
+});
 
 Route::middleware([
     'auth:sanctum',
